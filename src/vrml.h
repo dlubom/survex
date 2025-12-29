80df8f1ce75d01d9922b26f6f4668f8dce23a497
dlubom <darek.lubomski@gmail.com>
2025-12-29 17:40:27 +0100

    Add VRML export with date-threshold colouring

---
+++ src/vrml.h
@@ -0,0 +1,44 @@
+/* vrml.h
+ * Export from Aven as VRML.
+ */
+
+/* Copyright (C) 2025 Olly Betts
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef SURVEX_VRML_H
+#define SURVEX_VRML_H
+
+#include "exportfilter.h"
+
+class VRML : public ExportFilter {
+    int current_date = -1;
+    int date_threshold = 0;
+    int date_threshold_scheme = 0;
+
+  public:
+    VRML();
+    const int * passes() const override;
+    void header(const char *, time_t,
+		double min_x, double min_y, double min_z,
+		double max_x, double max_y, double max_z) override;
+    void set_leg_date(int date) override;
+    void line(const img_point *, const img_point *, unsigned, bool) override;
+    void label(const img_point *, const wxString&, int, int) override;
+    void footer() override;
+};
+
+#endif
